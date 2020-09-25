41f5142463091a924c53c6cfad10bc952b34a592
mat <mat>
2020-09-25 21:01:14 +0200

    adding olivers assets

--- web/assets/whoosh.js
+++ web/assets/whoosh.js
@@ -16,9 +16,35 @@ function updateTime(){
 }
 
 const toggleNavButton = document.querySelector('#navToggle');
-const mainNav = document.querySelector('#mainNav')
+const mobileNav = document.querySelector('#mobileNav');
+const eventsList = document.querySelector('#eventsList');
 if( toggleNavButton ){
     toggleNavButton.addEventListener('click', function(){
-        mainNav.classList.toggle('hidden');
+        mobileNav.classList.toggle('hidden');
     });
+}
+
+const showUpcomingEventsButton = document.querySelector('#showUpcomingEvents');
+const showPastEventsButton = document.querySelector('#showPastEvents');
+
+if( showUpcomingEventsButton && showPastEventsButton ){
+    showUpcomingEventsButton.addEventListener('click', function(){
+        showUpcomingEventsButton.classList.add('bg-blue', 'text-white');
+        showUpcomingEventsButton.classList.remove('bg-transparent', 'text-blue');
+
+        showPastEventsButton.classList.remove('bg-blue', 'text-white');
+        showPastEventsButton.classList.add('bg-transparent', 'text-blue');
+
+        eventsList.dataset.show = "upcoming";
+    });
+
+    showPastEventsButton.addEventListener('click', function (){
+        showUpcomingEventsButton.classList.remove('bg-blue', 'text-white');
+        showUpcomingEventsButton.classList.add('bg-transparent', 'text-blue');
+
+        showPastEventsButton.classList.add('bg-blue', 'text-white');
+        showPastEventsButton.classList.remove('bg-transparent', 'text-blue');
+
+        eventsList.dataset.show = "past";
+    })
 }
